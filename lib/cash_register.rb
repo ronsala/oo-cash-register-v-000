6fe01d8df167a66bcf0338e59345ec2ff81618ef
Ron Sala <ron.sala@gmail.com>
2018-02-01 06:32:23 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,23 +1,66 @@
+# class CashRegister
+#
+#   attr_accessor :total, :last_transaction
+#
+#   def initialize#(discount = nil)
+#     # cash_register = CashRegister.new
+#     @total = 0
+#   end
+#
+#   def cash_register_with_discount#(discount)
+#     initialize
+#     apply_discount
+#   end
+#
+#   def apply_discount#(discount)
+#     # @total =
+#   end
+#
+#   def void_last_transaction
+#
+#   end
+#
+# end
+
 class CashRegister
+  attr_accessor :discount, :total
 
-  attr_accessor :total, :last_transaction
 
-  def initialize#(discount = nil)
-    # cash_register = CashRegister.new
+  def initialize(discount = 0)
     @total = 0
+    @discount = discount
+    @cart = []
   end
 
-  def cash_register_with_discount#(discount)
-    initialize
-    apply_discount
+  def add_item(item, price, quantity = 1)
+    item_info = {}
+    item_info[:name] = item
+    item_info[:price] = price
+    item_info[:quantity] = quantity
+
+    @cart << item_info
+
+    @total += price * quantity
+
   end
 
-  def apply_discount#(discount)
-    # @total =
+  def apply_discount
+    if @discount == 0
+      return "There is no discount to apply."
+    end
+    @total -= @total * @discount / 100
+    return "After the discount, the total comes to $#{@total}."
   end
 
-  def void_last_transaction
-
+  def items
+    item_names = []
+    @cart.each do | item_info |
+      #test expects product name * quantity...
+      for qty in 1..item_info[:quantity]
+        item_names << item_info[:name]
+      end
+    end
+    item_names
   end
 
 end
