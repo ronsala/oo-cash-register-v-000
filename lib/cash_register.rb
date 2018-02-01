190e5cea698d6826faae3248e573de3048160197
Ron Sala <ron.sala@gmail.com>
2018-02-01 06:52:35 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,27 +1,3 @@
-# class CashRegister
-#
-#   attr_accessor :total, :last_transaction
-#
-#   def initialize#(discount = nil)
-#     # cash_register = CashRegister.new
-#     @total = 0
-#   end
-#
-#   def cash_register_with_discount#(discount)
-#     initialize
-#     apply_discount
-#   end
-#
-#   def apply_discount#(discount)
-#     # @total =
-#   end
-#
-#   def void_last_transaction
-#
-#   end
-#
-# end
-
 class CashRegister
   attr_accessor :discount, :total, :last_transaction
 
